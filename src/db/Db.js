dc0fff878a8938f27184bd1596b6a90823b630ac
saleem <saleem9590@gmail.com>
2022-11-30 23:13:28 +0530

    TodoApp

---
+++ src/db/Db.js
@@ -0,0 +1,8 @@
+const db= [
+    {
+        taskName:'SS',
+        date:'23/04/1998',
+        time:'11:45pm',
+        description:'SSSS'
+    }
+]
---
+++ src/modals/EditTask.js
@@ -0,0 +1,51 @@
+import React, { useState , useEffect} from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const EditTaskPopup = ({modal, toggle, updateTask, taskObj}) => {
+    const [taskName, setTaskName] = useState('');
+    const [date, setDate] = useState('');
+    const [description, setDescription] = useState('');
+
+    useEffect(() => {
+        setTaskName(taskObj.Name)
+        setDate(taskObj.date)
+        setDescription(taskObj.Description)
+    },[])
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        let tempObj = {}
+        tempObj['Name'] = taskName
+        tempObj['Date'] = date
+        tempObj['Description'] = description
+        updateTask(tempObj)
+    }
+
+    return (
+        <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Update Task</ModalHeader>
+            <ModalBody>
+            
+                    <div className = "form-group">
+                        <label>Task Name</label>
+                        <input type="text" className = "form-control" value = {taskName} onChange = {(e)=>setTaskName(e.target.value)} name = "taskName"/>
+                    </div>
+                    <div className = "form-group">
+                        <label>Date</label>
+                        <input type="date" className = "form-control" value = {date} onChange = {(e)=>setDate(e.target.value)} name = "date"/>
+                    </div>
+                    <div className = "form-group">
+                        <label>Description</label>
+                        <textarea rows = "5" className = "form-control" value = {description} onChange =  {(e)=>setDescription(e.target.value)} name = "description"></textarea>
+                    </div>
+                
+            </ModalBody>
+            <ModalFooter>
+            <Button color="primary" onClick={handleUpdate}>Update</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+      </Modal>
+    );
+};
+
+export default EditTaskPopup;
